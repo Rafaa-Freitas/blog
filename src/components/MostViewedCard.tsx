18075cdcd2c7519fcf76af52e1c25d63e96f9b2f
Rafael Freitas <rafael.freitasfa@gmail.com>
2024-10-17 23:40:52 -0300

    Adjusting MostViewedCard and deleting json database

--- src/components/MostViewedCard.tsx
+++ src/components/MostViewedCard.tsx
@@ -5,23 +5,25 @@ interface MostViewedCardProps {
 
 function MostViewedCard({ post }: MostViewedCardProps) {
   return (
-    <div className="w-grid-12 lg:w-grid-4 m-4 card">
+    <div className="w-grid-12 lg:w-grid-4 m-4 card flex flex-col">
       <div className="thumb overflow-hidden">
         <a href="">
           <img src={post.imageUrl} />
         </a>
       </div>
 
-      <div className="mt-4 px-4">
-        <h6 className="text-gray-5">{post.date}</h6>
+      <div className="mt-4 px-4 flex flex-col flex-1 justify-between">
+        <div>
+          <h6 className="text-gray-5">{post.date}</h6>
 
-        <h6 className="uppercase tracking-[2px] text-primary-light">
-          {post.category}
-        </h6>
+          <h6 className="uppercase tracking-[2px] text-primary-light">
+            {post.category}
+          </h6>
 
-        <h4>{post.title}</h4>
+          <h4>{post.title}</h4>
 
-        <p className="mt-2">{post.resume}</p>
+          <p className="mt-2">{post.resume}</p>
+        </div>
 
         <div className="my-6">
           <a href="" className="text-primary-light">
